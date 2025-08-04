9764fecb7fb0d1132c3c3f22c325811cfa66d550
Roman <craneamente@gmail.comm>
2025-08-03 23:34:51 -0500

    fix(api): Handle CORS preflight OPTIONS request

--- api/syro.js
+++ api/syro.js
@@ -2,6 +2,13 @@ const { GoogleAuth } = require('google-auth-library');
 
 // --- INICIO DE LA FUNCIÓN SERVERLESS ---
 export default async function handler(req, res) {
+  
+  // Manejar la solicitud preflight de CORS
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
